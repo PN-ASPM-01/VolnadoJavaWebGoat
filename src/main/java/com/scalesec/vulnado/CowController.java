a419259e0ff7df72f91b054e28d7014d91c8c84f
ComplianceLab <116727839+ComplianceLab@users.noreply.github.com>
2023-11-27 10:34:45 +0000

    Update CowController.java

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -13,6 +13,7 @@ public class CowController {
         return Cowsay.run(input);
     }
 public class CowController2 {
+    /* ali test */
     @RequestMapping(value = "/cowsay")
     String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
         return Cowsay.run(input);

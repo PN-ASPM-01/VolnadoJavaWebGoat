4169356d607e754ad10652afec89c2194208fe41
Rommy Bhuwaliya <104977830+Rommy-cbc@users.noreply.github.com>
2023-12-01 20:31:01 +0530

    Update CowController.java

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -12,10 +12,4 @@ public class CowController {
     String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
         return Cowsay.run(input);
     }
-public class CowController2 {
-    /* ali test */
-    @RequestMapping(value = "/cowsay")
-    String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
-        return Cowsay.run(input);
-    }
 }
